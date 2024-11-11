758f851a9421525cc6b22d309a5f0ade2d760b59
Pit <w.pit@hotmail.fr>
2024-11-11 11:24:08 +0100

    Reset

--- components/pressure_sensor/adc.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-
-void init_adc();
-float read_adc_voltage();
-
-#endif // ADC_H
--- components/pressure_sensor/pressure_sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRESSURE_SENSOR_H
-#define PRESSURE_SENSOR_H
-
-void init_pressure_sensor();
-float get_pressure();
-
-extern const int sampleInterval;
-
-#endif // PRESSURE_SENSOR_H
